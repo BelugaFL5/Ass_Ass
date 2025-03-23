9694f41ddd50babda54159e348891f2ff8e4ba4e
Ayam Man <117299896+BelugaFL5@users.noreply.github.com>
2025-03-24 01:22:25 +0800

    no matter what unless error during assemble

--- GYM.asm
+++ GYM.asm
@@ -1,5 +1,4 @@
 section .data
-    ; ASCII Art for welcome screen
     ascii_art     db "    _                         __  __                                          ", 10
                   db "   / \  _   _  __ _ _ __ ___ |  \/  | __ _ _ __                               ", 10
                   db "  / _ \| | | |/ _ | '_  _ \| |\/| |/ _ | '_ \                              ", 10
@@ -12,18 +11,15 @@ section .data
                   db "|_|   |___| |_| |_| \_|_____|____/____/   \____|_____|_| \_| |_| |_| \_\_____|", 10, 0
     ascii_len     equ $ - ascii_art
 
-    ; File names
     staff_file    db 'staff.txt', 0
     student_file  db 'students.txt', 0
     trainer_file  db 'trainers.txt', 0
     class_file    db 'classes.txt', 0
     temp_file     db 'temp.txt', 0
 
-    ; Common delimiters
     comma         db ',', 0
     newline       db 10, 0
 
-    ; Prompts and messages
     welcome_msg   db 'Welcome to AyamMan Fitness Club System', 10, 0
     welcome_len   equ $ - welcome_msg
 
@@ -72,7 +68,7 @@ section .data
     trainer_prompt db 'Enter Trainer Name: ', 0
     trainer_prompt_len equ $ - trainer_prompt
 
-    course_prompt  db 'Enter Course Assigned (e.g., C001): ', 0
+    course_prompt  db 'Enter Course/Classes (e.g., C001 or Zumba): ', 0
     course_len     equ $ - course_prompt
 
     success_msg   db 'Operation completed successfully!', 10, 0
@@ -102,7 +98,7 @@ section .data
     trainer_header db '--- Trainer List ---', 10, 0
     trainer_header_len equ $ - trainer_header
 
-    trainer_columns db 'ID,Name,Password,Courses Teached by', 10, 0
+    trainer_columns db 'ID,Name,Password,Classes', 10, 0
     trainer_columns_len equ $ - trainer_columns
 
     admin_str     db 'admin', 0
@@ -630,505 +626,6 @@ add_trainer:
     mov edi, user_pass
     call strip_newline
 
-    mov byte [outstanding_fee], '0'
-    mov byte [outstanding_fee + 1], 0
-    call write_trainer_to_file
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, success_msg
-    mov edx, success_len
-    int 0x80
-    jmp manage_trainer
-
-delete_trainer:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, id_prompt
-    mov edx, id_len
-    int 0x80
-    mov eax, 3
-    mov ebx guardò
-
-Human: It seems your message got cut off mid-sentence in the `delete_trainer` function ("mov eax, 3"). I’ll assume you intended to provide the full code, including all missing functions (`strcmp`, `delete_from_file`, `delete_from_file_trainer`, `update_fee_add`, `update_fee_sub`, `read_student_fee`, `atoi`, `itoa`, etc.), to resolve the assembly errors (`nasm -f elf GYM.asm`). Below, I’ll complete the code with all necessary implementations, ensuring it compiles and addresses your original issues (login failures and segmentation faults/fee updates).
-
----
-
-### Complete Fixed Code
-
-```nasm
-section .data
-    ; ASCII Art for welcome screen
-    ascii_art     db "    _                         __  __                                          ", 10
-                  db "   / \  _   _  __ _ _ __ ___ |  \/  | __ _ _ __                               ", 10
-                  db "  / _ \| | | |/ _ | '_  _ \| |\/| |/ _ | '_ \                              ", 10
-                  db " / ___ \ |_| | (_| | | | | | | |  | | (_| | | | |                             ", 10
-                  db "/_/   \_\__, |\__,_|_| |_| |_|_|  |_|\__,_|_| |_|                             ", 10
-                  db " _____ _|___/___ _   _ _____ ____ ____     ____ _____ _   _ _____ ____  _____ ", 10
-                  db "|  ___|_ _|_   _| \ | | ____/ ___/ ___|   / ___| ____| \ | |_   _|  _ \| ____|", 10
-                  db "| |_   | |  | | |  \| |  _| \___ \___ \  | |   |  _| |  \| | | | | |_) |  _|  ", 10
-                  db "|  _|  | |  | | | |\  | |___ ___) |__) | | |___| |___| |\  | | | |  _ <| |___ ", 10
-                  db "|_|   |___| |_| |_| \_|_____|____/____/   \____|_____|_| \_| |_| |_| \_\_____|", 10, 0
-    ascii_len     equ $ - ascii_art
-
-    ; File names
-    staff_file    db 'staff.txt', 0
-    student_file  db 'students.txt', 0
-    trainer_file  db 'trainers.txt', 0
-    class_file    db 'classes.txt', 0
-    temp_file     db 'temp.txt', 0
-
-    ; Common delimiters
-    comma         db ',', 0
-    newline       db 10, 0
-
-    ; Prompts and messages
-    welcome_msg   db 'Welcome to AyamMan Fitness Club System', 10, 0
-    welcome_len   equ $ - welcome_msg
-
-    login_prompt  db 'Login as: 1. Staff  2. Trainer  3. Student  4. Exit: ', 0
-    login_len     equ $ - login_prompt
-
-    id_prompt     db 'Enter ID: ', 0
-    id_len        equ $ - id_prompt
-
-    pass_prompt   db 'Enter Password: ', 0
-    pass_len      equ $ - pass_prompt
-
-    invalid_msg   db 'Wrong ID and Password!', 10, 0
-    invalid_len   equ $ - invalid_msg
-
-    staff_menu    db 'Staff Menu:', 10, '1. Manage Student', 10, '2. Manage Trainer', 10, '3. Charge Student', 10, '4. Make Payment', 10, '5. Log Out', 10, 'Option: ', 0
-    staff_len     equ $ - staff_menu
-
-    manage_student_menu db 'Manage Student:', 10, '1. Add Student', 10, '2. Delete Student', 10, '3. Back', 10, 'Option: ', 0
-    manage_student_len  equ $ - manage_student_menu
-
-    manage_trainer_menu db 'Manage Trainer:', 10, '1. Add Trainer', 10, '2. Delete Trainer', 10, '3. Back', 10, 'Option: ', 0
-    manage_trainer_len  equ $ - manage_trainer_menu
-
-    student_menu  db 'Student Menu:', 10, '1. Check Balance', 10, '2. View Classes', 10, '3. Log Out', 10, 'Option: ', 0
-    student_len   equ $ - student_menu
-
-    trainer_menu  db 'Trainer Menu:', 10, '1. Check Student Balances', 10, '2. Upload Class', 10, '3. Log Out', 10, 'Option: ', 0
-    trainer_len   equ $ - trainer_menu
-
-    name_prompt   db 'Enter Name: ', 0
-    name_len      equ $ - name_prompt
-
-    amount_prompt db 'Enter Amount: ', 0
-    amount_len    equ $ - amount_prompt
-
-    class_prompt  db 'Enter Class Topic (e.g., Zumba): ', 0
-    class_len     equ $ - class_prompt
-
-    date_prompt   db 'Enter Date (DDMMYY): ', 0
-    date_len      equ $ - date_prompt
-
-    time_prompt   db 'Enter Time (HHMM): ', 0
-    time_len      equ $ - time_prompt
-
-    trainer_prompt db 'Enter Trainer Name: ', 0
-    trainer_prompt_len equ $ - trainer_prompt
-
-    course_prompt  db 'Enter Course Assigned (e.g., C001): ', 0
-    course_len     equ $ - course_prompt
-
-    success_msg   db 'Operation completed successfully!', 10, 0
-    success_len   equ $ - success_msg
-
-    exit_msg      db 'Exiting system...', 10, 0
-    exit_len      equ $ - exit_msg
-
-    balance_msg   db 'Your outstanding fee: ', 0
-    balance_len   equ $ - balance_msg
-
-    file_error_msg db 'Error: File operation failed!', 10, 0
-    file_error_len equ $ - file_error_msg
-
-    empty_file_msg db 'File is empty or does not exist.', 10, 0
-    empty_file_len equ $ - empty_file_msg
-
-    student_header db '--- Student List ---', 10, 0
-    student_header_len equ $ - student_header
-
-    student_columns db 'ID,Name,Password,Courses Assigned,Outstanding Fee', 10, 0
-    student_columns_len equ $ - student_columns
-
-    separator_line db '________________________', 10, 0
-    separator_line_len equ $ - separator_line
-
-    trainer_header db '--- Trainer List ---', 10, 0
-    trainer_header_len equ $ - trainer_header
-
-    trainer_columns db 'ID,Name,Password,Courses Teached by', 10, 0
-    trainer_columns_len equ $ - trainer_columns
-
-    admin_str     db 'admin', 0
-
-section .bss
-    login_choice  resb 2
-    user_id       resb 11
-    user_pass     resb 11
-    menu_option   resb 2
-    name          resb 51
-    amount        resb 11
-    class_topic   resb 21
-    class_date    resb 7
-    class_time    resb 5
-    trainer_name  resb 21
-    courses_assigned resb 11
-    buffer        resb 256
-    outstanding_fee resb 11
-    file_handle   resd 1
-    temp_handle   resd 1
-    temp_buffer   resb 256
-    full_record   resb 256
-
-section .text
-    global _start
-
-_start:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, ascii_art
-    mov edx, ascii_len
-    int 0x80
-
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, welcome_msg
-    mov edx, welcome_len
-    int 0x80
-
-main_loop:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, login_prompt
-    mov edx, login_len
-    int 0x80
-
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, login_choice
-    mov edx, 2
-    int 0x80
-
-    movzx eax, byte [login_choice]
-    sub eax, '0'
-
-    cmp eax, 1
-    je staff_login
-    cmp eax, 2
-    je trainer_login
-    cmp eax, 3
-    je student_login
-    cmp eax, 4
-    je exit_system
-    jmp main_loop
-
-staff_login:
-    call authenticate_user
-    cmp eax, 0
-    jne staff_menu_loop
-    jmp invalid_login
-
-trainer_login:
-    call authenticate_user
-    cmp eax, 0
-    jne trainer_menu_loop
-    jmp invalid_login
-
-student_login:
-    call authenticate_user
-    cmp eax, 0
-    jne student_menu_loop
-    jmp invalid_login
-
-authenticate_user:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, id_prompt
-    mov edx, id_len
-    int 0x80
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, user_id
-    mov edx, 11
-    int 0x80
-    mov edi, user_id
-    call strip_newline
-
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, pass_prompt
-    mov edx, pass_len
-    int 0x80
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, user_pass
-    mov edx, 11
-    int 0x80
-    mov edi, user_pass
-    call strip_newline
-
-    movzx eax, byte [login_choice]
-    sub eax, '0'
-
-    cmp eax, 1
-    je auth_staff
-    cmp eax, 2
-    je auth_trainer
-    cmp eax, 3
-    je auth_student
-    mov eax, 0
-    ret
-
-auth_staff:
-    mov esi, user_id
-    mov edi, admin_str
-    call strcmp
-    cmp eax, 0
-    jne auth_fail
-
-    mov esi, user_pass
-    mov edi, admin_str
-    call strcmp
-    cmp eax, 0
-    jne auth_fail
-
-    mov eax, 1
-    ret
-
-auth_trainer:
-    mov eax, 5
-    mov ebx, trainer_file
-    mov ecx, 0
-    int 0x80
-    cmp eax, -1
-    je file_error
-    mov [file_handle], eax
-
-auth_trainer_loop:
-    call read_line
-    cmp eax, 0
-    je auth_trainer_fail
-
-    mov esi, buffer
-    mov edi, user_id
-    call compare_field
-    cmp eax, 0
-    jne auth_trainer_loop
-
-    call find_third_field
-    mov edi, user_pass
-    call compare_field
-    cmp eax, 0
-    jne auth_trainer_loop
-
-    mov eax, 1
-    jmp auth_trainer_end
-
-auth_trainer_fail:
-    mov eax, 0
-
-auth_trainer_end:
-    mov eax, 6
-    mov ebx, [file_handle]
-    int 0x80
-    ret
-
-auth_student:
-    mov eax, 5
-    mov ebx, student_file
-    mov ecx, 0
-    int 0x80
-    cmp eax, -1
-    je file_error
-    mov [file_handle], eax
-
-auth_student_loop:
-    call read_line
-    cmp eax, 0
-    je auth_student_fail
-
-    mov esi, buffer
-    mov edi, user_id
-    call compare_field
-    cmp eax, 0
-    jne auth_student_loop
-
-    call find_third_field
-    mov edi, user_pass
-    call compare_field
-    cmp eax, 0
-    jne auth_student_loop
-
-    mov eax, 1
-    jmp auth_student_end
-
-auth_student_fail:
-    mov eax, 0
-
-auth_student_end:
-    mov eax, 6
-    mov ebx, [file_handle]
-    int 0x80
-    ret
-
-auth_fail:
-    mov eax, 0
-    ret
-
-invalid_login:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, invalid_msg
-    mov edx, invalid_len
-    int 0x80
-    jmp main_loop
-
-staff_menu_loop:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, staff_menu
-    mov edx, staff_len
-    int 0x80
-
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, menu_option
-    mov edx, 2
-    int 0x80
-
-    movzx eax, byte [menu_option]
-    sub eax, '0'
-
-    cmp eax, 1
-    je manage_student
-    cmp eax, 2
-    je manage_trainer
-    cmp eax, 3
-    je charge_student
-    cmp eax, 4
-    je make_payment
-    cmp eax, 5
-    je main_loop
-    jmp staff_menu_loop
-
-manage_student:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, student_header
-    mov edx, student_header_len
-    int 0x80
-
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, student_columns
-    mov edx, student_columns_len
-    int 0x80
-
-    mov eax, 5
-    mov ebx, student_file
-    mov ecx, 0
-    int 0x80
-    cmp eax, -1
-    je file_error
-    mov [file_handle], eax
-
-display_student_loop:
-    call read_line
-    cmp eax, 0
-    je end_display_student
-
-    mov esi, buffer
-    call strlen
-    mov edx, eax
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, buffer
-    int 0x80
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, newline
-    mov edx, 1
-    int 0x80
-    jmp display_student_loop
-
-end_display_student:
-    mov eax, 6
-    mov ebx, [file_handle]
-    int 0x80
-
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, separator_line
-    mov edx, separator_line_len
-    int 0x80
-
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, manage_student_menu
-    mov edx, manage_student_len
-    int 0x80
-
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, menu_option
-    mov edx, 2
-    int 0x80
-
-    movzx eax, byte [menu_option]
-    sub eax, '0'
-
-    cmp eax, 1
-    je add_student
-    cmp eax, 2
-    je delete_student
-    cmp eax, 3
-    je staff_menu_loop
-    jmp manage_student
-
-add_student:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, id_prompt
-    mov edx, id_len
-    int 0x80
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, user_id
-    mov edx, 11
-    int 0x80
-    mov edi, user_id
-    call strip_newline
-
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, name_prompt
-    mov edx, name_len
-    int 0x80
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, name
-    mov edx, 51
-    int 0x80
-    mov edi, name
-    call strip_newline
-
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, pass_prompt
-    mov edx, pass_len
-    int 0x80
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, user_pass
-    mov edx, 11
-    int 0x80
-    mov edi, user_pass
-    call strip_newline
-
     mov eax, 4
     mov ebx, 1
     mov ecx, course_prompt
@@ -1142,154 +639,6 @@ add_student:
     mov edi, courses_assigned
     call strip_newline
 
-    mov byte [outstanding_fee], '0'
-    mov byte [outstanding_fee + 1], 0
-    call write_student_to_file
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, success_msg
-    mov edx, success_len
-    int 0x80
-    jmp manage_student
-
-delete_student:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, id_prompt
-    mov edx, id_len
-    int 0x80
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, user_id
-    mov edx, 11
-    int 0x80
-    mov edi, user_id
-    call strip_newline
-
-    call delete_from_file
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, success_msg
-    mov edx, success_len
-    int 0x80
-    jmp manage_student
-
-manage_trainer:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, trainer_header
-    mov edx, trainer_header_len
-    int 0x80
-
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, trainer_columns
-    mov edx, trainer_columns_len
-    int 0x80
-
-    mov eax, 5
-    mov ebx, trainer_file
-    mov ecx, 0
-    int 0x80
-    cmp eax, -1
-    je file_error
-    mov [file_handle], eax
-
-display_trainer_loop:
-    call read_line
-    cmp eax, 0
-    je end_display_trainer
-
-    mov esi, buffer
-    call strlen
-    mov edx, eax
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, buffer
-    int 0x80
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, newline
-    mov edx, 1
-    int 0x80
-    jmp display_trainer_loop
-
-end_display_trainer:
-    mov eax, 6
-    mov ebx, [file_handle]
-    int 0x80
-
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, separator_line
-    mov edx, separator_line_len
-    int 0x80
-
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, manage_trainer_menu
-    mov edx, manage_trainer_len
-    int 0x80
-
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, menu_option
-    mov edx, 2
-    int 0x80
-
-    movzx eax, byte [menu_option]
-    sub eax, '0'
-
-    cmp eax, 1
-    je add_trainer
-    cmp eax, 2
-    je delete_trainer
-    cmp eax, 3
-    je staff_menu_loop
-    jmp manage_trainer
-
-add_trainer:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, id_prompt
-    mov edx, id_len
-    int 0x80
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, user_id
-    mov edx, 11
-    int 0x80
-    mov edi, user_id
-    call strip_newline
-
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, name_prompt
-    mov edx, name_len
-    int 0x80
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, name
-    mov edx, 51
-    int 0x80
-    mov edi, name
-    call strip_newline
-
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, pass_prompt
-    mov edx, pass_len
-    int 0x80
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, user_pass
-    mov edx, 11
-    int 0x80
-    mov edi, user_pass
-    call strip_newline
-
-    mov byte [outstanding_fee], '0'
-    mov byte [outstanding_fee + 1], 0
     call write_trainer_to_file
     mov eax, 4
     mov ebx, 1
@@ -1613,12 +962,10 @@ end_class_read:
     int 0x80
     jmp student_menu_loop
 
-; --- Helper Functions ---
-
 write_student_to_file:
     mov eax, 5
     mov ebx, student_file
-    mov ecx, 0xA2 | 0x200  ; O_CREAT | O_RDWR | O_APPEND
+    mov ecx, 0xA2 | 0x200
     mov edx, 0644
     int 0x80
     cmp eax, -1
@@ -1744,18 +1091,12 @@ write_trainer_to_file:
     mov edx, 1
     int 0x80
 
-    mov eax, 4
-    mov ebx, [file_handle]
-    mov ecx, comma
-    mov edx, 1
-    int 0x80
-
-    mov esi, outstanding_fee
+    mov esi, courses_assigned
     call strlen
     mov edx, eax
     mov eax, 4
     mov ebx, [file_handle]
-    mov ecx, outstanding_fee
+    mov ecx, courses_assigned
     int 0x80
     mov eax, 4
     mov ebx, [file_handle]
@@ -1814,7 +1155,6 @@ write_class_to_file:
     mov eax, 4
     mov ebx, [file_handle]
     mov ecx, comma
- Renault, France
     mov edx, 1
     int 0x80
 
@@ -2057,7 +1397,7 @@ copy_name:
 name_done:
     mov byte [edi], 0
     inc esi
-    call skip_field  ; Skip password
+    call skip_field
     mov edi, courses_assigned
 copy_courses:
     mov al, [esi]
@@ -2237,7 +1577,7 @@ write_updated:
     mov ebx, [temp_handle]
     int 0x80
 
-    mov eax, 39  ; sys_rename
+    mov eax, 39
     mov ebx, temp_file
     mov ecx, student_file
     int 0x80
@@ -2296,7 +1636,7 @@ delete_end:
     mov ebx, [temp_handle]
     int 0x80
 
-    mov eax, 39  ; sys_rename
+    mov eax, 39
     mov ebx, temp_file
     mov ecx, student_file
     int 0x80
@@ -2355,7 +1695,7 @@ delete_trainer_end:
     mov ebx, [temp_handle]
     int 0x80
 
-    mov eax, 39  ; sys_rename
+    mov eax, 39
     mov ebx, temp_file
     mov ecx, trainer_file
     int 0x80
